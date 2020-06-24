230cd3aac1d70cb57b46ba5a35b412a2bbe7c686
Yulia Potapova <potapovaym@yandex.ru>
2020-06-24 17:16:57 +0400

    Этап 2. Бэкенд - Аутентификация и авторизация

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,11 @@
-// const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-// const { NODE_ENV, JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const User = require('../models/user');
-// const NotFoundError = require('../errors/notFoundError');
-// const ConflictError = require('../errors/conflictError');
+const NotFoundError = require('../errors/notFoundError');
+const ConflictError = require('../errors/conflictError');
 
 module.exports.getUser = (req, res, next) => {
   User.find({})
@@ -13,30 +13,30 @@ module.exports.getUser = (req, res, next) => {
     .catch(next);
 };
 
-// module.exports.createUser = (req, res, next) => {
-//   const {
-//     name, about, avatar, email, password,
-//   } = req.body;
-//   bcrypt.hash(password, 10)
-//     .then((hash) => User.create({
-//       name, about, avatar, email, password: hash,
-//     }))
-//     .then((user) => res.send({ data: user.omitPrivate() }))
-//     .catch((err) => {
-//       if (err.code === 11000) {
-//         next(new ConflictError('Пользователь с таким e-mail уже зарегистрирован'));
-//       } else {
-//         next(err);
-//       }
-//     });
-// };
+module.exports.createUser = (req, res, next) => {
+  const {
+    email, password, name
+  } = req.body;
+  bcrypt.hash(password, 10)
+    .then((hash) => User.create({
+      email, password: hash, name,
+    }))
+    .then((user) => res.send({ data: user.omitPrivate() }))
+    .catch((err) => {
+      if (err.code === 11000) {
+        next(new ConflictError('Пользователь с таким e-mail уже зарегистрирован'));
+      } else {
+        next(err);
+      }
+    });
+};
 
-// module.exports.login = (req, res, next) => {
-//   const { email, password } = req.body;
-//   return User.findUserByCredentials(email, password)
-//     .then((user) => {
-//       const token = jwt.sign({ _id: user._id }, (NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'), { expiresIn: '7d' });
-//       res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
-//     })
-//     .catch(next);
-// };
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign({ _id: user._id }, (NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret'), { expiresIn: '7d' });
+      res.cookie('jwt', token, { maxAge: 3600000 * 24 * 7, httpOnly: true }).end();
+    })
+    .catch(next);
+};
